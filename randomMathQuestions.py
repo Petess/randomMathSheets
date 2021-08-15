074fae8dfc2e101265fc6099d4635071b1799c93
U-PETEXEON\User <petess@gmail.com>
2021-08-15 16:04:02 +1000

    Added different operations

--- randomMathQuestions.py
+++ randomMathQuestions.py
@@ -2,10 +2,12 @@
 
 from random import * 
 
-
 def GenerateSingleDigits(numberToGenerate):
+    symbols = ['+','-','*']
+
     for j in range(0,numberToGenerate):
-        print(str(randint(1,9)) + " + " + str(randint(1,9)) + " =    " ,
+        
+        print(str(randint(1,9)) + symbols[randint(0,2)] + str(randint(1,9)) + " =    " ,
               end='')
 
 if __name__ == "__main__":
